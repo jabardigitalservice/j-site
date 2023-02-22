e25f269d88cd4272f7ebc1f87db31cca45d0499c
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2023-02-22 10:26:42 +0700

    feat (navbar): create basic navbar

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="relative" :style="style">
+    <Navbar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar.vue'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
